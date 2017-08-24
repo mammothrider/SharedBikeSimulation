1f38d1d0eedaa25b5527088ba922a19b39fc1b8b
Mammothrider <xushubin1992@gmail.com>
2017-08-24 21:41:57 +0900

    算法部分

--- Simulation.py
+++ Simulation.py
@@ -1,7 +1,7 @@
 #Simulation
 #根据用户位置信息生成站点并计算站点最优位置
 #算法：
-#假设站点有最大服务距离
+#假设站点有最大距离，用成本进行计算
 #所有未注册过的点计算出一个最佳位置的新站点N，不考虑服务距离
 #所有点计算一轮成本，并在车站注册，考虑服务距离
 #如果成本变化小于特定值，如0.5，返回最后的成本
@@ -13,20 +13,28 @@ userPosition = [[0, 0]]
 def loadUserPosition():
 	pass
 	
-def minimumCostPosition(userList):
+def massCenter(userList):
 	if (!userList)
 		return null;
 	x, y = zip(*(userList))
 	x, y = sum(x)/len(x), sum(y)/len(y)
 	return [x, y]
 	
-def _utestminimumCostPosition():
+def _utestMassCenter():
 	a = [[1, 1], [2, 2], [3, 3], [-1, -1], [-2, -2], [-3, -3]]
 	print(minimumCostPosition(a))
 	
+def distance(p1, p2):
+	if (!p1 || !p2)
+		return null
+		
+	x = p1[0] - p2[0]
+	y = p1[1] - p2[1]
+	return x * x + y * y
+	
+
 	
 if __name__ == '__main__':
-	_utestminimumCostPosition()
-	
+	# _utestMassCenter()
 
 	
